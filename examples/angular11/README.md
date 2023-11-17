ec1b374749d086172e82d1d780cea715846d7adf
Łukasz Ciesielski <lookasc.i@gmail.com>
2023-11-17 12:46:18 +0100

    Add examples for angular 11 and 17

---
+++ examples/angular11/README.md
@@ -0,0 +1,23 @@
+# ngx-translate-phraseapp Demo
+
+This demo project shows how to integrate the [Phrase In-Context Editor](https://phrase.com/) into a Angular app using [ngx-translate](https://github.com/ngx-translate/core) for localization via the [ngx-translate-phraseapp](https://github.com/phrase/ngx-translate-phraseapp) plugin.
+
+## Install
+
+    $ npm install
+
+## Configure
+
+The demo project uses the project ID from the [Phrase demo project](http://demo.phrase.com/). If you want to use the [ngx-translate adapter](https://github.com/phrase/ngx-translate-phraseapp) in your own project, make sure to adjust the project ID accordingly in 'src/app/app.component.ts'.
+
+## Run
+
+Start the server:
+
+    $ npm start
+
+and then open the app at: [localhost:4200](http://localhost:4200)
+
+## Get help / support
+
+Please contact [support@phrase.com](mailto:support@phrase.com?subject=[GitHub]%20) and we can take more direct action toward finding a solution.
---
+++ examples/angular17/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule, TranslateLoader, TranslateCompiler } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { PhraseAppCompiler } from "ngx-translate-phraseapp";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: PhraseAppCompiler
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
